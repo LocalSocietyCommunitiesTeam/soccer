8444d00412a73c24f28b0807169a1285aff61793
地域社会との絆チーム <147232004+LocalSocietyCommunitiesTeam@users.noreply.github.com>
2024-05-22 11:51:49 +0900

    jsのイベント短縮化

--- quiz/quiz_files/css/App.js
+++ quiz/quiz_files/css/App.js
@@ -1,53 +1,32 @@
 /** 共通 **/
 /* コンポーネント：ローダー */
-// ローダーを表示
-function showLoader() {
-    const loader = document.getElementsByClassName('com_loader')[0];
+if (document.getElementsByClassName('com_loader')) {
+    // ローダーを表示
+    function showLoader() {
+        const loader = document.getElementsByClassName('com_loader')[0];
 
-    loader.classList.remove('com_loader_hidden');
+        loader.classList.remove('com_loader_hidden');
+    }
+
+    // ローダーを非表示
+    function hideLoader() {
+        const loader = document.getElementsByClassName('com_loader')[0];
+
+        loader.classList.add('com_loader_hidden');
+    }
 }
-
-// ローダーを非表示
-function hideLoader() {
-    const loader = document.getElementsByClassName('com_loader')[0];
-
-    loader.classList.add('com_loader_hidden');
-}
-
-// クイズDBのWEBアプリURL
-const QUIZ_DB_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwekpsKlbmS5MX75qUJh-YSjES8wkDGW-FejUZR7QDE0SkXURWGt8fHZIvMEZGmYIU-2g/exec';
-
 
 window.addEventListener('DOMContentLoaded', function () {
+    // クイズDBのWEBアプリURL
+    const QUIZ_DB_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwekpsKlbmS5MX75qUJh-YSjES8wkDGW-FejUZR7QDE0SkXURWGt8fHZIvMEZGmYIU-2g/exec';
     // クイズデータの取得
     fetchfunction(QUIZ_DB_WEB_APP_URL);
 
-});
-
-async function fetchfunction(url) {
-    await fetch(url).then((response) => {
-        return response.json();
-    }).then((data) => {
-        return JSON.parse(data);
-    }).then((jsonData) => {
-        setQuizData(jsonData);
-        hideLoader();
-    }).catch((error) => {
-        console.log('error: ' + error);
-        // window.alert('通信エラーが発生しました。\n電波が良い場所でもう一度ページを開いてください。\n\n解決しない場合は管理者に問い合わせてください。\n管理者: meijiyasuda.kizuna@gmail.com');
-    });
-}
-
-function setQuizData(quizData) {
-    const quizNum = document.getElementById('sqq_quizNum');
-    const question = document.getElementById('sqq_question');
-    const optionList = document.getElementById('sqq_optionList');
-}
-
-/** ホーム画面 **/
-window.addEventListener('DOMContentLoaded', function () {
-    const homeSec = document.getElementsByClassName('com_homeSec')[0];
-    const goToLoginBtn = document.getElementById('sqh_goToLoginBtn');
+    // 要素を取得
+    const homeSec = document.getElementsByClassName('com_homeSec')[0]; // ホームセクション
+    const goToLoginBtn = document.getElementById('sqh_goToLoginBtn'); // ログイン画面へボタン
+    const userName = document.getElementById('sql_userName'); // ユーザー名テキストフィールド
+    const loginBtn = document.getElementById('sql_loginBtn'); // ログインボタン
 
     homeSec.addEventListener('click', function () {
         const goToLoginBtn = document.getElementById('sqh_goToLoginBtn');
@@ -66,12 +45,6 @@ window.addEventListener('DOMContentLoaded', function () {
         homeSec.classList.add('com_secHidden');
         loginSec.classList.remove('com_secHidden');
     });
-});
-
-/** ログイン画面 **/
-window.addEventListener('DOMContentLoaded', function () {
-    const userName = document.getElementById('sql_userName');
-    const loginBtn = document.getElementById('sql_loginBtn');
 
     userName.addEventListener('keydown', function (e) {
         if (e.key === 'Enter') {
@@ -102,6 +75,26 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+async function fetchfunction(url) {
+    await fetch(url).then((response) => {
+        return response.json();
+    }).then((data) => {
+        return JSON.parse(data);
+    }).then((jsonData) => {
+        setQuizData(jsonData);
+        hideLoader();
+    }).catch((error) => {
+        console.log('error: ' + error);
+        // window.alert('通信エラーが発生しました。\n電波が良い場所でもう一度ページを開いてください。\n\n解決しない場合は管理者に問い合わせてください。\n管理者: meijiyasuda.kizuna@gmail.com');
+    });
+}
+
+function setQuizData(quizData) {
+    const quizNum = document.getElementById('sqq_quizNum');
+    const question = document.getElementById('sqq_question');
+    const optionList = document.getElementById('sqq_optionList');
+}
+
 function isFilled(element) {
     if (element.value.trim().length > 0) {
         return true;
@@ -110,5 +103,7 @@ function isFilled(element) {
     }
 }
 
+/** ホーム画面 **/
+/** ログイン画面 **/
 /** クイズ画面 **/
 /** 結果画面 **/
