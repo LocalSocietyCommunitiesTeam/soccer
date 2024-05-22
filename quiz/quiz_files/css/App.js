9cccfaf75523d23f1dd9258ca534e5a4e106104e
地域社会との絆チーム <147232004+LocalSocietyCommunitiesTeam@users.noreply.github.com>
2024-05-22 12:55:20 +0900

    ログインエラー

--- quiz/quiz_files/css/App.js
+++ quiz/quiz_files/css/App.js
@@ -22,19 +22,43 @@ window.addEventListener('DOMContentLoaded', function () {
     // クイズデータの取得
     fetchfunction(QUIZ_DB_WEB_APP_URL);
 
+    async function fetchfunction(url) {
+        await fetch(url).then((response) => {
+            return response.json();
+        }).then((data) => {
+            return JSON.parse(data);
+        }).then((jsonData) => {
+            setQuizData(jsonData);
+            hideLoader();
+        }).catch((error) => {
+            console.log('error: ' + error);
+            // window.alert('通信エラーが発生しました。\n電波が良い場所でもう一度ページを開いてください。\n\n解決しない場合は管理者に問い合わせてください。\n管理者: meijiyasuda.kizuna@gmail.com');
+        });
+    }
+
+    // クイズデータを画面にセット
+    function setQuizData(quizData) {
+        window.alert('quizData:\n' + quizData);
+        const quizNum = document.getElementById('sqq_quizNum');
+        const question = document.getElementById('sqq_question');
+        const optionList = document.getElementById('sqq_optionList');
+    }
+
     // 要素を取得
     const homeSec = document.getElementsByClassName('com_homeSec')[0]; // ホームセクション
     const goToLoginBtn = document.getElementById('sqh_goToLoginBtn'); // ログイン画面へボタン
     const userName = document.getElementById('sql_userName'); // ユーザー名テキストフィールド
-    const loginBtn = document.getElementById('sql_loginBtn'); // ログインボタン
+    const startBtn = document.getElementById('sql_loginBtn'); // クイズ開始ボタン
 
     homeSec.addEventListener('click', function () {
+        window.alert('ホームセクションをクリック');
         const goToLoginBtn = document.getElementById('sqh_goToLoginBtn');
 
         goToLoginBtn.click();
     });
 
     goToLoginBtn.addEventListener('click', function (e) {
+        window.alert('ログイン画面へボタンをクリック');
         e.stopPropagation();
 
         window.navigator.vibrate(200);
@@ -48,60 +72,55 @@ window.addEventListener('DOMContentLoaded', function () {
 
     userName.addEventListener('keydown', function (e) {
         if (e.key === 'Enter') {
-            const userName = document.getElementById('userName');
+            window.alert('ユーザー名テキストフィールドでEnterキーを押下');
 
-            userName.blur();
+            this.blur();
             this.click();
         }
     });
 
-    loginBtn.addEventListener('click', function () {
-        showLoader();
+    // クイズ開始ボタンを押下
+    startBtn.addEventListener('click', function () {
+        const loginSec = document.getElementsByClassName('com_loginSec')[0];
+        const quizSec = document.getElementsByClassName('com_quizSec')[0];
+        const userName = document.getElementById('sql_userName');
+        const loginErrorArea = document.getElementsByClassName('sql_loginErrorArea')[0];
 
-        const userName = document.getElementById('userName');
         if (isFilled(userName)) {
-            const loginSec = document.getElementsByClassName('com_loginSec')[0];
-            const quizSec = document.getElementsByClassName('com_quizSec')[0];
+            // ローダーを表示
+            showLoader();
+
+            // ログインエラーエリアを非表示
+            loginErrorArea.classList.add('sql_loginErrorHidden');
 
             userName.parentElement.classList.remove('com_textField_error');
             loginSec.classList.add('com_secHidden');
             quizSec.classList.remove('com_secHidden');
+
+            /* ここでデータを登録 */
+            // ユーザー名
+            // ログイン日時
+
         } else {
             userName.value = '';
             userName.parentElement.classList.add('com_textField_error');
+            // ログインエラーエリアを表示
+            loginErrorArea.classList.remove('sql_loginErrorHidden');
         }
 
+        // ローダーを非表示
         hideLoader();
     });
-});
 
-async function fetchfunction(url) {
-    await fetch(url).then((response) => {
-        return response.json();
-    }).then((data) => {
-        return JSON.parse(data);
-    }).then((jsonData) => {
-        setQuizData(jsonData);
-        hideLoader();
-    }).catch((error) => {
-        console.log('error: ' + error);
-        // window.alert('通信エラーが発生しました。\n電波が良い場所でもう一度ページを開いてください。\n\n解決しない場合は管理者に問い合わせてください。\n管理者: meijiyasuda.kizuna@gmail.com');
-    });
-}
-
-function setQuizData(quizData) {
-    const quizNum = document.getElementById('sqq_quizNum');
-    const question = document.getElementById('sqq_question');
-    const optionList = document.getElementById('sqq_optionList');
-}
-
-function isFilled(element) {
-    if (element.value.trim().length > 0) {
-        return true;
-    } else {
-        return false;
+    // 要素の文字数が1文字以上か判定
+    function isFilled(element) {
+        if (element.value.trim().length > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
+});
 
 /** ホーム画面 **/
 /** ログイン画面 **/
