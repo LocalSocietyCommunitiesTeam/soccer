c2d5e65ef3a82d87bb30ec9cf05f891564a444d5
地域社会との絆チーム <147232004+LocalSocietyCommunitiesTeam@users.noreply.github.com>
2024-05-30 16:14:30 +0900

    テストのpost

--- quiz/quiz_files/css/test.js
+++ quiz/quiz_files/css/test.js
@@ -3,6 +3,24 @@ const WEBAPPURL = 'https://script.google.com/macros/s/AKfycbx0ghxxDOCkNzh1E1aeFk
 
 var quizData;
 var logData;
+var userData = {
+    "userId": "1234567890",
+    "loginDate": "2024/05/30 15:46:30",
+    "answerDate": "2024/05/30 15:48:27",
+    "choice": [
+        4,
+        3,
+        1,
+        2,
+        3,
+        1,
+        4,
+        2,
+        1,
+        2
+    ],
+    "point": 8
+};
 
 function addParametersToURL(url, params) {
     // URLオブジェクトを作成
@@ -96,6 +114,34 @@ async function fetchUserLog(userId) {
     }
 }
 
+async function logAnswer(userId, loginDate, answerDate, choice, point) {
+    const newURL = addParametersToURL(WEBAPPURL, { action: 'setUserLog' });
+
+    try {
+        const response = await fetch(newURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userId,
+                loginDate,
+                answerDate,
+                choice,
+                point
+            }),
+            timeout: 10000
+        });
+
+        const data = response.json();
+        
+        return data;
+    } catch (e) {
+        console.error(e.name, e.message);
+    }
+}
+
+
 window.addEventListener('DOMContentLoaded', function () {
 
     const quizBtn = document.getElementById('quizBtn');
@@ -103,6 +149,9 @@ window.addEventListener('DOMContentLoaded', function () {
     const random10QuizBtn = document.getElementById('random10QuizBtn');
     const allLogBtn = document.getElementById('allLogBtn');
     const userLogBtn = document.getElementById('userLogBtn');
+    const postUserLogBtn = document.getElementById('postUserLogBtn');
+
+    const date = new Date();
 
     quizBtn.addEventListener('click', function () {
         console.log('quizBtn');
@@ -129,4 +178,25 @@ window.addEventListener('DOMContentLoaded', function () {
         // fetchUserLog('A123456');
         fetchUserLog('A876771');
     });
+
+    postUserLogBtn.addEventListener('click', function () {
+        console.log('postUserLogBtn');
+        fetchUserLog(userData.userId, userData.loginDate, userData.answerDate, userData.choice, userData.point);
+    });
 });
+
+
+function formatDate(date) {
+    let output = '';
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    output = year + '/' + month + '/' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+
+    return output;
+}
